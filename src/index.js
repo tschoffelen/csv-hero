f3e71319fc4670fcac5335ee59b69bddbc4a83e8
Thomas Schoffelen <thomas@schof.co>
2021-09-03 14:50:54 +0100

    Fix reference to obsolete CSS file.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import '@nadavshaar/react-grid-table/dist/index.css';
 import './index.scss';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
